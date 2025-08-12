35ef5c7c9aded0c97c3ddca0355dc0d26dab0cc4
Avijit Tarafder <avijittarafder22@gmail.com>
2025-08-12 10:05:07 +0600

    Update user.py

--- models/user.py
+++ models/user.py
@@ -4,3 +4,9 @@ from app.db.database import Base
 class User(Base):
     __tablename__ = "users"
   
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_admin = Column(Boolean, default=False)
