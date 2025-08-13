cc3331b47ccbe3e836565d35d3afde4620b63d9c
Avijit Tarafder <avijittarafder22@gmail.com>
2025-08-13 10:13:53 +0600

    Update menu.py

--- routers/menu.py
+++ routers/menu.py
@@ -2,3 +2,14 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.db.database import SessionLocal
 from app.models.menu import MenuItem
+
+router = APIRouter(prefix="/menu", tags=["Menu"])
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+  
