d230fc118ff33816fd232f72547bd7f276d08698
Avijit Tarafder <avijittarafder22@gmail.com>
2025-08-17 08:24:39 +0600

    Update order.py

--- routers/order.py
+++ routers/order.py
@@ -6,3 +6,18 @@ from app.schemas.order import OrderCreate, OrderOut
 
 router = APIRouter(prefix="/orders", tags=["Orders"])
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/", response_model=OrderOut)
+def create_order(order: OrderCreate, db: Session = Depends(get_db)):
+    new_order = Order(user_id=1, item_ids=",".join(map(str, order.item_ids)))
+    db.add(new_order)
+    db.commit()
+    db.refresh(new_order)
+    return new_order
+
