6b3cbc0b9b6df125370c0d4c3a3dcfc30ffb8e5c
Avijit Tarafder <avijittarafder22@gmail.com>
2025-08-20 11:00:16 +0600

    Create user.py

---
+++ schemsa/user.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    username: str
+    email: str
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    username: str
+    email: str
+    is_admin: bool
+
+    class Config:
+        orm_mode = True
+      
